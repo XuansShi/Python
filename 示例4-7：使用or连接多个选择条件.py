59dab222cb26cb07963e8c72c3414eb1641ba017
XuanShi <z3356782747@163.com>
2024-12-31 19:12:35 +0800

    示例4-7：使用or连接多个选择条件

---
+++ 示例4-7：使用or连接多个选择条件.py
@@ -0,0 +1,36 @@
+#示例4-7：使用or连接多个选择条件
+score=eval(input('请输入您的成绩：'))
+if score<0 or score>100:
+    print('成绩无效')
+else:
+    print('您的成绩为:',score)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
